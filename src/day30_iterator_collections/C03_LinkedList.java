c0bc8562c6a1419240cdb5d0afe355f321b1ce06
EsraAdileYilmaz <yilmazesraadile@gmail.com>
2023-10-20 20:25:19 +0200

    day30_collections

--- src/day30_iterator_collections/C03_LinkedList.java
+++ src/day30_iterator_collections/C03_LinkedList.java
@@ -1,9 +1,6 @@
 package day30_iterator_collections;
 
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class C03_LinkedList {
 
@@ -48,11 +45,43 @@ public class C03_LinkedList {
         //ama element olarak 8'i silmek icin=once bir variable'a 8'i atamamiz lazim.
         //sonra onu remove() methodu ile sileriz.
         Integer silinecekSayi= 8;
-        System.out.println(ll2.remove(silinecekSayi));//true (yani bu elementi sildim)
+        System.out.println(ll2.remove(silinecekSayi));//true (yani bu elementi buldum,sildim)
+        silinecekSayi=12;
+        System.out.println(ll2.remove(silinecekSayi));//false (yani bu elementi bulamadim,silemedim)
         System.out.println(ll2);//[4, 5]
 
 
+        List<Integer> list=new ArrayList<>();
+        list.add(4);
+        list.add(6);
+        list.add(9);
+        list.add(10);
+        System.out.println(list);//[4, 6, 9, 10]
 
+        System.out.println(list.retainAll(ll2));//true
+        System.out.println("ll2 :"+ll2);//ll2 :[4, 5] hicbir degisiklik olmadi
+        System.out.println("list :"+list);//list :[4] bu list tamamen degisti.sadece her iki list icinde ortak olan eleman kaldi
+
+        /*
+            retainAll() methodu iki list uzerinde yapilir.
+            iki listin ortak elemanlarina bakilir.
+            ilk listte sadece ortak elemanlar kalir,gerisi silinir.
+            ikinci listte ise hicbir degisiklik olmaz.
+            ornegin;System.out.println(list.retainAll(ll2));
+            list'den ll2'da olmayan elemanlari siler
+            eger herhangi bir elemani silerse true
+            hic bir elemani silemezse false doner.
+         */
+
+        list.add(6);
+        list.add(9);
+        list.add(10);
+        System.out.println(list.removeAll(ll2));
+        System.out.println("ll2 :"+ll2);//ll2 :[4, 5]
+        System.out.println("list :"+list);//list :[6, 9, 10] icindeki her iki list'te bulunan ortak elemanlar silindi.
+
+        // retainAll() ortak elemanlar disindakileri siler
+        // removeAll() ise ortak elemanlari siler
 
 
 
---
+++ src/day30_iterator_collections/C04_Queue.java
@@ -0,0 +1,68 @@
+package day30_iterator_collections;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class C04_Queue {
+
+    public static void main(String[] args) {
+
+        /*
+        Queue'nun methodlari=
+        1)add() sona element ekler.
+        2)remove() elementleri bastan baslayarak siler
+        3)element() methodu queue'nun ilk elemanini silmeden bize getirir.bulamazsa exception firlatir
+        4)peek() methodu queue'nun ilk elemanini silmeden bize getirir.bulamazsa null dondurur
+        5)offer() methodu eger kapasite sinirlamasi yoksa, verdigimiz elementi queue'ya ekler
+        6)poll() methodu Queue'nun ilk elementini siler ve bize dondurur
+
+        add() methodu ile offer() methodu arasindaki fark=
+        add() methodu verilen elementi her halukarda eklerken,
+        offer() methodu kapasite uygunsa ekler.
+
+         */
+
+        Queue <String> harfler=new LinkedList<>();
+        //Queue interface'inin ozelliklerini alan bir obje olusturduk.
+        harfler.add("y");
+        harfler.add("K");
+        harfler.add("M");
+        harfler.add("s");
+        System.out.println(harfler);//[y, K, M, s]
+
+        System.out.println(harfler.remove());//y (ilk indextekini siler)
+        System.out.println(harfler);//[K, M, s]
+        //tekrar remove() yapsak K silinir
+        //tekrar remove() yapsak M silinir
+        //tekrar remove() yapsak s silinir.Sirayla siliyor.
+
+        harfler.add("M");
+        harfler.add("M");
+
+        System.out.println(harfler.remove("M"));//true (yani M'yi buldum,sildim)
+        System.out.println(harfler);//[K, s, M, M] ilk M silindi
+
+        //element() methodu queue'nun ilk elemanini silmeden bize getirir.bulamazsa exception firlatir
+        System.out.println(harfler.element());//K
+        System.out.println(harfler);//[K, s, M, M]
+
+        //peek() methodu queue'nun ilk elemanini silmeden bize getirir.bulamazsa null dondurur
+        System.out.println(harfler.peek());//K
+        System.out.println(harfler);//[K, s, M, M]
+
+        //element() method ile peek() method arasindaki fark, bos bir Queue'da kullanildiginda gorulur
+        Queue <String> deneme=new LinkedList<>();//[]
+        //System.out.println(deneme.element());//NoSuchElementException (boyle bir element yok)
+        System.out.println(deneme.peek());//null
+
+        // offer() methodu eger kapasite sinirlamasi yoksa, verdigimiz elementi queue'ya ekler
+        System.out.println(harfler.offer("Z"));//sona ekleme yapti
+        System.out.println(harfler);//[K, s, M, M, Z]
+
+
+        // poll() methodu Queue'nun ilk elementini siler ve bize dondurur
+        System.out.println(harfler.poll());//K
+        System.out.println(harfler);//[s, M, M, Z]
+
+    }
+}
